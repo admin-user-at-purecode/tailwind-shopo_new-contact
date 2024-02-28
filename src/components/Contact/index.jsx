09d6c6f95c696c3403cb470b765aa70e7c534060
Akhil Bolla <akhilbolla89@gmail.com>
2024-02-28 19:18:52 +0530

    deleted form

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -17,8 +17,8 @@ export default function Contact() {
       </div>
       <div className="contact-wrapper w-full mb-10">
         <div className="container-x mx-auto">
-          <div className="main-wrapper w-full lg:flex lg:space-x-[30px]">
-            <div className="lg:w-1/2 w-full">
+          <div className="main-wrapper w-full lg:flex lg:space-x-[30px] ">
+            <div className="w-full">
               <h1 className="text-[22px] font-semibold text-qblack leading-[30px] mb-1">
                 Contact Information
               </h1>
@@ -27,8 +27,8 @@ export default function Contact() {
                 possible.
               </p>
 
-              <div className="xl:flex xl:space-x-[30px] mb-[30px]">
-                <div className="xl:w-1/2 w-full  h-[196px] flex flex-col item justify-center bg-[#FFEAE5] p-5">
+              <div className="flex flex-col mb-[30px] w-100 gap-4">
+                <div className="w-full  h-[196px] flex flex-col item justify-center bg-[#FFEAE5] p-5">
                   <div className="flex justify-center mb-3 ">
                   <ThinPeople/>
                   </div>
@@ -42,7 +42,7 @@ export default function Contact() {
                     +(434) 5466 5467 443
                   </p>
                 </div>
-                <div className="xl:w-1/2 w-full h-[196px] flex flex-col item justify-center bg-[#D3EFFF] p-5">
+                <div className=" w-full h-[196px] flex flex-col item justify-center bg-[#D3EFFF] p-5">
                   <div className="flex justify-center mb-3 ">
                   <ThinPeople/>
                   </div>
@@ -57,7 +57,7 @@ export default function Contact() {
                   </p>
                 </div>
               </div>
-              <div className="p-5 flex flex-col justify-between w-full bg-[#E7F2EC]">
+              <div className="p-5 flex flex-col  w-full bg-[#E7F2EC]">
                 <div className="flex space-x-5">
                 <ThinPeople/>
                   <div>
@@ -78,53 +78,6 @@ export default function Contact() {
                     allowFullScreen=""
                     loading="lazy"
                   ></iframe>
-                </div>
-              </div>
-            </div>
-            <div className="flex-1 bg-white sm:p-10 p-3">
-              <div className="title flex flex-col items-center">
-                <h1 className="text-[34px] font-bold text-qblack">
-                  Get In Touch
-                </h1>
-              </div>
-              <div className="inputs mt-5">
-                <div className="mb-4">
-                  <InputCom
-                    label="Frist Name*"
-                    placeholder="Demo Name"
-                    name="first_name"
-                    inputClasses="h-[50px]"
-                  />
-                </div>
-                <div className="mb-4">
-                  <InputCom
-                    label="Email Address*"
-                    placeholder="info@quomodosoft.com"
-                    name="email"
-                    inputClasses="h-[50px]"
-                  />
-                </div>
-                <div className="mb-4">
-                  <InputCom
-                    label="Subject*"
-                    placeholder="Your Subject here"
-                    name="subject"
-                    inputClasses="h-[50px]"
-                  />
-                </div>
-                <div className="mb-5">
-                  <h6 className="input-label text-qgray capitalize text-[13px] font-normal block mb-2 ">
-                    Message*
-                  </h6>
-                  <textarea
-                    placeholder="Type your message here"
-                    className="w-full h-[105px] focus:ring-0 focus:outline-none p-3 border border-qgray-border placeholder:text-sm"
-                  ></textarea>
-                </div>
-                <div>
-                    <div className="black-btn text-sm font-semibold w-full h-[50px] flex justify-center items-center">
-                      <span>Send Now</span>
-                    </div>
                 </div>
               </div>
             </div>
