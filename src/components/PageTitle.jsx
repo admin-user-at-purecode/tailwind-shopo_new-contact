caffb636511f9b96460eef07de7d6593febf5b57
Akhil Bolla <akhilbolla89@gmail.com>
2024-03-01 15:35:51 +0530

    la5

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -3,11 +3,11 @@ import BreadcrumbCom from "./BreadcrumbCom";
 export default function PageTitle({ title, breadcrumb = [] }) {
   return (
     <div className="page-title-wrapper bg-[#FFFAEF] w-full h-[173px] py-10 ">
-      <div className="container-x mx-auto">
-        <div className="mb-5">
+      <div className=" flex justify-center flex-col items-center">
+        <div className="mb-4">
           <BreadcrumbCom paths={breadcrumb} />
         </div>
-        <div className="flex justify-center">
+        <div className="">
           <h1 className="text-4xl font-semibold text-qblack">{title}</h1>
         </div>
       </div>
