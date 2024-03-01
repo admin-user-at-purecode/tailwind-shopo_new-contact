1147f38f7e1bc72588ab02ed0825075a5dbb9d8f
Akhil Bolla <akhilbolla89@gmail.com>
2024-03-01 11:13:51 +0530

    cd5

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -18,69 +18,6 @@ export default function Contact() {
       <div className="contact-wrapper w-full mb-10">
         <div className="container-x mx-auto">
           <div className="main-wrapper w-full lg:flex lg:space-x-[30px]">
-            <div className="lg:w-1/2 w-full">
-              <h1 className="text-[22px] font-semibold text-qblack leading-[30px] mb-1">
-                Contact Information
-              </h1>
-              <p className="text-[15px] text-qgraytwo leading-[30px] mb-5">
-                Fill the form below or write us .We will help you as soon as
-                possible.
-              </p>
-
-              <div className="xl:flex xl:space-x-[30px] mb-[30px]">
-                <div className="xl:w-1/2 w-full  h-[196px] flex flex-col item justify-center bg-[#FFEAE5] p-5">
-                  <div className="flex justify-center mb-3 ">
-                  <ThinPeople/>
-                  </div>
-                  <p className="text-[22px] text-black leading-[30px] text-center font-semibold">
-                    Phone
-                  </p>
-                  <p className="text-[15px] text-black leading-[30px] text-center">
-                    +(323) 9847 3847 383
-                  </p>
-                  <p className="text-[15px] text-black leading-[30px] text-center">
-                    +(434) 5466 5467 443
-                  </p>
-                </div>
-                <div className="xl:w-1/2 w-full h-[196px] flex flex-col item justify-center bg-[#D3EFFF] p-5">
-                  <div className="flex justify-center mb-3 ">
-                  <ThinPeople/>
-                  </div>
-                  <p className="text-[22px] text-black leading-[30px] text-center font-semibold">
-                    Email
-                  </p>
-                  <p className="text-[15px] text-black leading-[30px] text-center">
-                    Demoemail@gmail.com
-                  </p>
-                  <p className="text-[15px] text-black leading-[30px] text-center">
-                    rafiqulislamsuvobd@gmail.com
-                  </p>
-                </div>
-              </div>
-              <div className="p-5 flex flex-col justify-between w-full bg-[#E7F2EC]">
-                <div className="flex space-x-5">
-                <ThinPeople/>
-                  <div>
-                    <h1 className="text-[22px] font-semibold text-qblack leading-[30px] mb-2">
-                      Address
-                    </h1>
-                    <p className="text-[15px] text-qblack leading-[30px]">
-                      4517 Washington Ave. Manchester, Road 2342, <br />
-                      Kentucky 39495
-                    </p>
-                  </div>
-                </div>
-                <div className="w-full h-[206px] mt-5">
-                  <iframe
-                    title="newWork"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.94539481518!2d-74.26675559025064!3d40.69739290398433!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1656755618576!5m2!1sen!2sbd"
-                    style={{ border: "0", width: "100%", height: "100%" }}
-                    allowFullScreen=""
-                    loading="lazy"
-                  ></iframe>
-                </div>
-              </div>
-            </div>
             <div className="flex-1 bg-white sm:p-10 p-3">
               <div className="title flex flex-col items-center">
                 <h1 className="text-[34px] font-bold text-qblack">
